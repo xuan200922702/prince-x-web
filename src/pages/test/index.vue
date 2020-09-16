ef041a62a3af5e048f5751960476264466550659
huangmengbo <1217157081@qq.com>
2020-09-16 14:29:03 +0800

    dev

---
+++ src/pages/test/index.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div class="test">test</div>
+
+</template>
+
+
+<script>
+export default {
+  name: "test",
+  data () {
+   return {}
+  },
+   methods:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
